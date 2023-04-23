f5e879652e44c9e53b52f890c95b6ac7ec7beedb
MustafaArabaci <mustafaarabaci2007@gmail.com>
2023-04-23 12:09:37 +0300

    Artan Sayaç Yapımı Html Css Js

---
+++ Artan Sayaç Yapımı/main.js
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach((counter) => {
+  counter.innerHTML = '0'
+
+  const updateCounter = () => {
+    const target = Number(counter.getAttribute('data-target'))
+
+    const c = +counter.innerText
+
+    const increment = target / 1000
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 1)
+    } else {
+      counter.innerText = target
+    }
+  }
+
+  updateCounter()
+})
